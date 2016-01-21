2a200dee5da88347d92738323e50b6dfb5b50a59
nishitki <sk8erdude@outlook.com>
2016-01-21 14:18:25 +0900

    Update build_rpm.rb

--- build_rpm.rb
+++ build_rpm.rb
@@ -6,25 +6,13 @@ require 'open3'
 require 'fileutils'
 Bundler.require
 
-repo ='vcjp/packages'
+repo ='packages'
 branch ="#{ENV['GIT_BRANCH']}"
 svnconf='/var/lib/jenkins/workspace/build-vc-packages/.subversion/'
-pullrequesturl = ''
+pullrequest_url = ''
+pullrequest_num = ENV['ghprbPullId']
 
-client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
-pull = client.pull_requests(repo, :state => 'open')
-pull.each do |p|
-  pullrequesturl = p.html_url.gsub(/api.github.com\/repos/, 'github.com').gsub(/pulls/, 'pull')
-  pullrequestnum = p.number
-  head = p.head.ref
-    if !head.eql?(branch)
-      puts "branch name does not mutch git head"
-      exit (1)
-    end
-end
-
-#ENV['HOME'] = "#{ENV['WORKSPACE']}/#{ENV['BUILD_NUMBER']}"
-ENV['HOME'] = "#{ENV['WORKSPACE']}/#{pullrequestnum}"
+ENV['HOME'] = "#{ENV['WORKSPACE']}/#{pullrequest_num}"
 ENV['JAVA_HOME'] = '/usr/local/java'
 ENV['ANT_HOME'] = '/usr/local/apache-ant-1.7.0'
 ENV['PATH'] = "#{ENV['PATH']}:#{ENV['ANT_HOME']}/bin"
@@ -41,7 +29,7 @@ end
 def post(text)
   data = {
     "channel"  => '#infra',
-    "username" => 'hubot',
+    "username" => '',
     "icon_url" => 'https://avatars3.githubusercontent.com',
     "text" => text
   }
@@ -50,30 +38,53 @@ def post(text)
   http = Net::HTTP.post_form(uri, {"payload" => data.to_json})
 end
 
+client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+pull = client.pull_requests(repo, :state => 'open')
+pull.each do |p|
+  pullrequest_url = p.html_url.gsub(/api.github.com\/repos/, 'github.com').gsub(/pulls/, 'pull')
+  head = p.head.ref
+  if !head.eql?(branch)
+    puts "branch name does not mutch git head"
+    exit (1)
+  end
+
+end
+
 FileUtils.mkdir_p("#{ENV['HOME']}") unless FileTest.exist?("#{ENV['HOME']}")
 FileUtils.mkdir_p("#{ENV['HOME']}/.subversion") unless FileTest.exist?("#{ENV['HOME']}/.subversion")
 FileUtils.cp_r( svnconf, "#{ENV['HOME']}" , preserve: true )
 
 Open3.popen3("rpmdev-setuptree") do |stdin, stdout, stderr, wait_thr|
-  unless stderr.read.empty?
-    puts "ERROR: Can\'t create build environment"
-    exit (1)
+    unless stderr.read.empty?
+      puts "ERROR: Can\'t create build environment"
+      exit (1)
+    end
+end
+
+pull_files = client.pull_request_files(repo, pullrequest_num)
+files = []
+pull_files.each do |p|
+  if p.filename.match(/.*spec$/) then
+    files.push(p.filename)
   end
 end
 
-Open3.popen3("rpmbuild --clean -ba `ls -t *.spec |head -1`") do |stdin, stdout, stderr, wait_thr|
-  while output = stdout.gets
-    output.chomp!
-    puts output
-  end
-  unless wait_thr.value.success?
-    exit (1)
+files.each do |f|
+  #Open3.popen3("rpmbuild --clean -ba `ls -t *.spec |head -1`") do |stdin, stdout, stderr, wait_thr|
+  Open3.popen3("rpmbuild --clean -ba #{f}") do |stdin, stdout, stderr, wait_thr|
+    while output = stdout.gets
+      output.chomp!
+      puts output
+    end
+    unless wait_thr.value.success?
+      exit (1)
+    end
   end
 end
 
 body = <<-"EOC"
 Pull Request: master -> #{ENV['GIT_BRANCH']} build successfully finished
-continue manual merge #{pullrequesturl} to deploy
+continue manual merge #{pullrequest_url} to deploy
 just close pull request to cancel
 EOC
 
